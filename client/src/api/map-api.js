944abbe9ac62acf9bd4f142cd9228a2bb3f308e5
ShauralP <patel675@purdue.edu>
2021-07-04 18:33:47 -0700

    fix(client/api): use correct path for proxy

--- client/src/api/map-api.js
+++ client/src/api/map-api.js
@@ -3,21 +3,18 @@ import axios from "axios";
 const _mapApi = "/api/map/";
 
 async function getCountries() {
-  axios.defaults.baseURL = "localhost:8000/";
   const url = `${_mapApi}country`;
   const response = await axios.get(url);
   return response.data;
 }
 
 async function getAllStates() {
-  axios.defaults.baseURL = "localhost:8000/";
   const url = `${_mapApi}state`;
   const response = await axios.get(url);
   return response.data;
 }
 
 async function getStates(country) {
-  axios.defaults.baseURL = "localhost:8000/";
   const url = `${_mapApi}state/${country}`;
   const response = await axios.get(url);
   return response.data;
